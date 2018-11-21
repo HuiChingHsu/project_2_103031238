acb50c5bf55f6cf3b68612476ccc6ce3dc5307b4
HuiChingHsu <sunny27223@hotmail.com>
2018-11-21 15:29:42 +0800

    Version_2_11/21

--- project2.cpp
+++ project2.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <vector>
 #include <limits.h>
+#include <queue>
 
 using namespace std;
 
@@ -33,7 +34,7 @@ int main(int argc, char* argv[])
         file >> col_bound;
         file >> battery;
 
-//cout << row_bound << " " << col_bound << " " << battery << endl;
+        position robot, start_pos;
  
         for(int i=0; i<row_bound; i++){  // creating the map
             vector<int> Row;
@@ -42,19 +43,53 @@ int main(int argc, char* argv[])
                 int num;
                 file >> value;
                 if(value == '0') num = 0;
-                else if(value == '1')num = 1;
-                else if(value == 'R') num = INT_MAX;
-//cout << num <<" ";
+                else if(value == '1') num = 1;
+                else if(value == 'R'){
+                    robot.row = i, start_pos.row = i;
+                    robot.col = j, start_pos.col = j;
+                    num = INT_MIN;
+                }
                 Row.push_back(num);
             }
-//cout << endl;
             Map.push_back(Row);
         }    
         
     }else{
         cout << "Unable to open file" << endl;
         return 0;
-    }
+    } //end of inputting data (Map -> visiting state)
+
+    position*** adjList;
+    int x;
+    for(int i=0; i<row_bound; i++){
+        for(int j=0; j<col_bound; j++){
+            x = 0;
+            if(Map[i][j] == 0 || Map[i][j] == INT_MIN){
+                if(i>0 && Map[i-1][j] == 0){
+                    adjList[i][j][x].row = i-1;
+                    adjList[i][j][x].col = j;
+                    x++;
+                }
+                if(i<row_bound-1 && Map[i+1][j] == 0){
+                    adjList[i][j][x].row = i+1;
+                    adjList[i][j][x].col = j;
+                    x++;
+                }
+                if(j>0 && Map[i][j-1] == 0){
+                    adjList[i][j][x].row = i;
+                    adjList[i][j][x].col = j-1;
+                    x++;
+                }
+                if(j<col_bound-1 && Map[i][j+1] == 0){
+                    adjList[i][j][x].row = i;
+                    adjList[i][j][x].col = j+1;
+                }
+            }
+            
+        }
+    }//end of creating an adjcency list
+
+    queue route;
 
     //output result
     /*
