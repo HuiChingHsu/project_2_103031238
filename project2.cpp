9ed6c73ebf411edac72068d26a5fde2829c0797b
HuiChingHsu <sunny27223@hotmail.com>
2018-11-22 10:35:04 +0800

    version_3_11/22

--- project2.cpp
+++ project2.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <limits.h>
 #include <queue>
+#include <list>
 
 using namespace std;
 
@@ -12,7 +13,30 @@ class position
 public:
     int row;
     int col;
+    position operator = (const position& p){
+        position P;
+        P.row = p.row;
+        P.col = p.col;
+        return P;
+    }
 };
+
+/*void BFS(position s, int * map, position *** adj)
+{
+    list<position> Queue;
+    map[s.row][s.col] += 2;
+    Queue.push_back(s);
+
+    while(!Queue.empty()){
+        s = Queue.front();
+        if(map[s.row][s.col] != INT_MIN+2) cout << s.row << " " << s.col;
+        Queue.pop_front();
+
+        int x=0;
+        for(int i=0; adj[i][j][x]; )
+    }
+}*/
+
 int main(int argc, char* argv[])
 {
     int row_bound, col_bound, battery;
@@ -59,10 +83,17 @@ int main(int argc, char* argv[])
         return 0;
     } //end of inputting data (Map -> visiting state)
 
-    position*** adjList;
+    position*** adjList = new position** [row_bound];
     int x;
     for(int i=0; i<row_bound; i++){
+            adjList[i] = new position* [col_bound];
         for(int j=0; j<col_bound; j++){
+            adjList[i][j] = new position [4];
+            
+            for(int k=0; k<4; k++){
+                adjList[i][j][k].row = INT_MAX;
+                adjList[i][j][k].col = INT_MAX;
+            }  //initializing adjList;
             x = 0;
             if(Map[i][j] == 0 || Map[i][j] == INT_MIN){
                 if(i>0 && Map[i-1][j] == 0){
@@ -89,27 +120,25 @@ int main(int argc, char* argv[])
         }
     }//end of creating an adjcency list
 
-    queue route;
+    for(int i=0; i<4; i++){
+        cout << adjList[1][1][i].row << " " << adjList[1][1][i].col << endl;
+    }
 
     //output result
     /*
     data_name = "./"+id+"./final.peak";
     output.open(data_name,ios::out);
-
     output << ans_num << "\n";
     if (output.is_open()) {
-
         while(!result.empty()){
             output << result.front().row << " " << result.front().col << "\n";
             result.pop();
         }
-
 	}
 	else{
         cout << "Unable to open file" << endl;
         return 0;
     } 
-
 	file.close();
 	output.close();
     */
